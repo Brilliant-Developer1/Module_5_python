b7d3938c0b7d7c777b596ccc0ead1ac17422b234
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-10-16 21:27:44 +0600

    Phone file added

---
+++ calculator.py
@@ -0,0 +1,6 @@
+
+class Calculator:
+    brand = 'Casio'
+
+    def add(sel, num1, num2):
+        pass
--- phone.py
+++ phone.py
@@ -1,8 +1,23 @@
 
-class Nokia:
+
+def call():
+    print('Calling')
+    return 'Call Done'
+
+class Phone:
     price = 20000
     color = 'Black'
     brand = "Nokia"
+    features = ['camera', 'speaker', 'Hatori']
 
-myPhone = Nokia() 
-print(myPhone.brand, myPhone.price)   
+    def call(self):
+        print('Calling Person - 1')
+    def send_sms(self, phone,sms):
+        text = f'Sending SMS to: {phone} and message: {sms}'  
+        return text  
+
+my_phone = Phone() 
+print(my_phone.brand)
+my_phone.call()   
+res = my_phone.send_sms(5566,'Hello there') 
+print(res)
